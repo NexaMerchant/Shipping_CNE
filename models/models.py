7c99c913b6b1886bcb32fd060f462c8c8d6161ed
odoo <nice.lizhi@gmail.com>
2024-12-26 05:28:21 +0000

    add action for order handler

--- models/models.py
+++ models/models.py
@@ -17,6 +17,8 @@ class shipping_cne(models.Model):
     shipping_currency = fields.Char()
     shipping_status = fields.Char()
     shipping_description = fields.Text()
+    order_id = fields.Many2one('sale.order', string='Order')
+    shipping_code = fields.Many2one('stock.picking', string='Shipping')
     shipping_user = fields.Many2one('res.users', string='User')
     shipping_partner = fields.Many2one('res.partner', string='Partner')
 
@@ -32,4 +34,28 @@ class shipping_cne(models.Model):
     @api.model
     def _get_default_partner(self):
         return self.env['res.partner'].browse(self._uid)
+    
+    @api.model
+    def _get_default_order(self):
+        return self.env['sale.order'].browse(self._uid)
+    
+    # get nce shipping code
+    @api.model
+    def _get_default_code(self):
+        return self.env['stock.picking'].browse(self._uid)
+    
+    # create a new shipping for the order
+    @api.model
+    def create_shipping(self):
+        return self.env['shipping_cne.shipping_cne'].create({
+            'order_id': self._get_default_order().id,
+            'shipping_code': self._get_default_code().id,
+            'shipping_user': self._get_default_user().id,
+            'shipping_partner': self._get_default_partner().id,
+        })
+    
+    # get the shipping code
+    @api.model
+    def get_shipping_code(self):
+        return self._get_default_code().id
     
